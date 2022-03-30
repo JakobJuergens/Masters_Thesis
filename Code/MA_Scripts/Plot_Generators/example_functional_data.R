b6803ef3e4f1d75da2b8b31d4ade11bd183fbea2
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-03-31 00:07:34 +0200

    Added growth curves

---
+++ Code/MA_Scripts/Plot_Generators/example_functional_data.R
@@ -0,0 +1,45 @@
+# This script generates a plot that gives a visiual example of functional
+# data to introduce the concept from an intuitive perspective.
+
+library(tidyverse)
+library(fda)
+
+data(growth)
+
+male_growth_tibble <- cbind(
+  'Age' = rownames(growth$hgtm),
+  'Group' = 'male',
+  as_tibble(growth$hgtm)) %>%
+  pivot_longer(cols = !c(Age, Group), names_to = 'ID', values_to = 'Height')
+
+female_growth_tibble <- cbind(
+  'Age' = rownames(growth$hgtf),
+  'Group' = 'female',
+  as_tibble(growth$hgtf)) %>%
+  pivot_longer(cols = !c(Age, Group), names_to = 'ID', values_to = 'Height')
+
+growth_tibble <- rbind(male_growth_tibble, female_growth_tibble) %>%
+  mutate(Age = as.numeric(Age),
+         Height = as.numeric(Height))
+
+# create plot
+growth_plot <- ggplot(data = growth_tibble) +
+  geom_line(aes(x = Age, y = Height, col = Group, group = ID),
+            alpha = 0.5) +
+  theme_light() +
+  labs(y = 'Height in cm') +
+  theme(
+    legend.position = c(.05,.85),
+    legend.text = element_text(size = 20),
+    legend.title = element_text(size = 30),
+    plot.title = element_text(size = 30),
+    axis.title = element_text(size = 20),
+    axis.text = element_text(size = 20),
+  ) +
+  guides(color = guide_legend(override.aes = list(lwd = 5)))
+
+# save plot in appropriate folder
+ggsave(
+  filename = "../../Graphics/growth_curves.PDF", plot = growth_plot,
+  width = 20, height = 6, units = "in", dpi = 600
+)
