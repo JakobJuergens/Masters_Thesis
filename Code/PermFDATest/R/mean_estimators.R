3aa2521c74dd1c62e3de99cf123a0161457c3f7a
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-04-09 21:00:03 +0200

    More Mean-Estimation

--- Code/PermFDATest/R/mean_estimators.R
+++ Code/PermFDATest/R/mean_estimators.R
@@ -1,3 +1,33 @@
+#' This function finds the estimated mean function from a given sample
+#'
+#' @param sample: sample, specified as a list where each element is one observation
+#' @param interpolation_mode: string that determines the mode of interpolation between
+#' discrete measurement points. Options: 'linear', 'fourier', 'eigen'
+#' @param domain: vector with beginning and endpoint of the closed interval
+#' that is the domain of the stochastic processes
+#' @param n_basis: if interpolation mode is a basis based method,
+#' this determines the number of basis functions used in the approximation
+#' @param grid: grid used for the approximation of the mean function
+#'
+#' @return Placeholder
+mean_estimator <- function(sample, interpolation_mode = "linear",
+                           domain = c(0, 1), n_basis = NA, grid) {
+  # These functions can be found in the file mean_estimators.R
+  if (interpolation_mode == "linear") {
+    return(linear_mean_estimator(sample = sample, domain = domain, grid = grid))
+  }
+  if (interpolation_mode == "fourier") {
+    return(fourier_mean_estimator(sample = sample, domain = domain, n_basis = n_basis, grid = grid))
+  }
+  if (interpolation_mode == "eigen") {
+    return(eigenbasis_mean_estimator(sample = sample, domain = domain, n_basis = n_basis, grid = grid))
+  } else {
+    (
+      stop("Chosen Interpolation Mode is not available.")
+    )
+  }
+}
+
 #' This function finds the estimated mean function from a given sample using
 #' linear interpolation between the discrete measurement points
 #'
@@ -93,8 +123,10 @@ linear_interpolation <- function(observation, grid) {
 #' @return An estimate of the mean function in the usual observation format
 fourier_mean_estimator <- function(sample, domain, n_basis, grid) {
   # Create Fourier Basis of chosen size
-  fourier_basis <- fda::create.fourier.basis(rangeval = domain, nbasis = n_basis,
-                                             period = domain[2] - domain[1])
+  fourier_basis <- fda::create.fourier.basis(
+    rangeval = domain, nbasis = n_basis,
+    period = domain[2] - domain[1]
+  )
   # Evaluate basis at grid points
   fourier_basis_eval <- fda::eval.basis(evalarg = grid, basisobj = fourier_basis)
   # Fit sample using Fourier Basis
@@ -127,6 +159,7 @@ fourier_mean_estimator <- function(sample, domain, n_basis, grid) {
 
 #' This function finds the estimated mean function from a given sample using
 #' an approximation using a truncated Eigenbasis (Karhunen-Loeve Representation)
+#' Currently non-functional!
 #'
 #' @param sample: sample, specified as a list where each element is one observation
 #' @param domain: vector with beginning and endpoint of the closed interval
@@ -135,6 +168,36 @@ fourier_mean_estimator <- function(sample, domain, n_basis, grid) {
 #' @param grid: grid used for the approximation of the mean function
 #'
 #' @return An estimate of the mean function in the usual observation format
-eigenbasis_mean_estimator <- function(sample, domain, n_basis, grid){
-
+eigenbasis_mean_estimator <- function(sample, domain, n_basis, grid) {
+  # Create cubic bspline Basis with many functions
+  bspline_basis <- fda::create.bspline.basis(rangeval = domain, nbasis = 100, norder = 4)
+  # Fit sample using Fourier Basis
+  fitted_sample <- purrr::map(
+    .x = sample,
+    .f = function(obs) {
+      fda::smooth.basis(
+        argvals = obs$args,
+        y = obs$vals,
+        fdParobj = bspline_basis
+      )$fd
+    }
+  )
+  # combine fd objects for fpca
+  combined_fd_obj <- fda::fd(
+    coef = matrix(
+      data = unlist(
+        purrr::map(.x = fitted_sample, .f = function(obs) obs$coefs)
+      ),
+      nrow = 100, ncol = length(sample), byrow = FALSE
+    ),
+    basis = fitted_sample[[1]]$basis
+  )
+  # conduct fpca (centerfns doesn't make sense here because we're looking for
+  # the mean, but this is just for testing purposes)
+  sample_fpca <- fda::pca.fd(fdobj = combined_fd_obj, nharm = n_basis, centerfns = TRUE)
+  # return mean object
+  return(list(
+    args = grid,
+    vals = fda::eval.fd(evalarg = grid, fdobj = sample_fpca$meanfd)
+  ))
 }
--- Code/PermFDATest/R/means_test.R
+++ Code/PermFDATest/R/means_test.R
@@ -1,30 +1,3 @@
-#' This function finds the estimated mean function from a given sample
-#'
-#' @param sample: sample, specified as a list where each element is one observation
-#' @param interpolation_mode: string that determines the mode of interpolation between
-#' discrete measurement points. Options: 'linear', 'fourier'
-#' @param domain: vector with beginning and endpoint of the closed interval
-#' that is the domain of the stochastic processes
-#' @param n_basis: if interpolation mode is a basis based method,
-#' this determines the number of basis functions used in the approximation
-#' @param grid: grid used for the approximation of the mean function
-#'
-#' @return Placeholder
-mean_estimator <- function(sample, interpolation_mode = "linear",
-                           domain = c(0, 1), n_basis = NA, grid) {
-  # These functions can be found in the file mean_estimators.R
-  if (interpolation_mode == "linear") {
-    return(linear_mean_estimator(sample = sample, domain = domain, grid = grid))
-  }
-  if (interpolation_mode == "fourier") {
-    return(fourier_mean_estimator(sample = sample, domain = domain, n_basis = n_basis, grid = grid))
-  } else {
-    (
-      stop("Chosen Interpolation Mode is not available.")
-    )
-  }
-}
-
 #' This function performs the mean-based test for two samples with a chosen
 #' set of parameters.
 #'
