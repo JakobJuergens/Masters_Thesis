938edd691688b6b5af97b58fcbeea66fdcbbcbd1
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-04-12 23:06:08 +0200

    Structure for empirical dist functions

---
+++ Code/PermFDATest/R/empirical_distribution_function.R
@@ -0,0 +1,25 @@
+#' This function checks if one function is bigger than another function for all
+#' points in a closed interval
+#'
+#' @param func_a: Function one
+#' @param func_b: Function two
+#' @param domain: interval to check
+#'
+#' @return TRUE or FALSE depending on wheter func_a is always bigger than
+#' func_b
+func_comparison <- function(func_a, func_b, domain = c(0,1)){
+
+}
+
+#' Returns the value of an empirical distribution function of a given sample
+#' for a given function
+#'
+#' @param sample: Sample of functional data
+#' @param func_b: Function to evaluate the empirical distribution function at
+#' @param domain: Domain of the functions in both the sample and the comparison
+#' function
+#'
+#' @return An approximated value of the empirical distribution function
+empirical_dist_func <- function(sample, func, domain = c(0,1)){
+
+}
