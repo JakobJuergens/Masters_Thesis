e027f8085b3cb55cdd0f42044953267ce3d8dacd
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-04-20 18:53:10 +0200

    Added potential Application

---
+++ Code/MA_Scripts/Application/Electricity_Consumption.R
@@ -0,0 +1,16 @@
+library(fds)
+
+# load data for weekdays
+weekday_demand <- list(
+  mon_demand = fds::mondaydemand,
+  tue_demand = fds::tuesdaydemand,
+  wed_demand = fds::wednesdaydemand,
+  thu_demand = fds::thursdaydemand,
+  fri_demand = fds::fridaydemand)
+
+# load data for weekends
+weekend_demand <- list(
+  sat_demand = fds::saturdaydemand,
+  sun_demand = fds::sundaydemand
+)
+
