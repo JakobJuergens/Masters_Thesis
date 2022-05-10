8abb7a94fd760481f0d88a1904a95408175ea1bc
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-05-10 20:21:39 +0200

    Some structure for Test Function

---
+++ Code/PermFDATest/R/general_perm_test_functions.R
@@ -0,0 +1,11 @@
+#' This function performs the task of the permutation test function (called phi
+#' in the corresponding paper)
+#'
+#' @param alpha: Significance Level of the test
+#' @param crit_value: critical value of the permutation test
+#' @param realized_value: realized value of the test statistic
+#'
+#' @return: The value of the permutation test function
+perm_test <- function(alpha, crit_value, realized_value){
+
+}
