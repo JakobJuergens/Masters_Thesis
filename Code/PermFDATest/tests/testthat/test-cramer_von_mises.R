51702e41b2bfbf258d8849f58f75b36f4309be30
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-05-11 12:45:24 +0200

    Test Restructuring

--- Code/PermFDATest/tests/testthat/test-cramer_von_mises.R
+++ Code/PermFDATest/tests/testthat/test-cramer_von_mises.R
@@ -34,3 +34,44 @@ test_that("Cramer-von Mises Test gives output in the correct format", {
   expect_equal(object = is.numeric(tau_hat), expected = TRUE)
   expect_equal(object = (tau_hat >= 0), expected = TRUE)
 })
+
+test_that("Cramer-von Mises Test gives output for full variant of permutation procedure.", {
+  # choose weight function
+  w_func <- function(x) {
+    return(1)
+  }
+  # choose rho
+  rho <- seq(from = 10, to = 1, length.out = 15)
+  # generate basis
+  basis <- fda::create.fourier.basis(rangeval = c(0, 1), nbasis = 15, period = 1)
+  # generate test_samples:
+  grid <- seq(0, 1, length.out = 100)
+  sample_1 <- quick_funcify(sample = rep(
+    x = list(list(
+      args = grid,
+      vals = rep(1, times = length(grid))
+    )), times = 3
+  ), domain = c(0, 1), n_fourier_basis = 15)
+
+  sample_2 <- quick_funcify(sample = rep(
+    x = list(list(
+      args = grid,
+      vals = rep(-5, times = length(grid))
+    )), times = 3
+  ), domain = c(0, 1), n_fourier_basis = 15)
+
+  Q_1 <- choose(n = length(sample_1) + length(sample_2), k = length(sample_1))
+
+  tau_hats <- perm_tstats_full(
+    Q = Q_1, sample1 = sample_1, sample2 = sample_2,
+    t_stat_func = cramer_von_mises_tstat, domain = c(0, 1),
+    type = 'fourier', basis = fourier_basis, w_func = w_func,
+    rho = rho, u_sample_func = u_unif, n_func = 5
+  )
+
+  crit_value_1 <- crit_val(realizations = tau_hats, alpha = 0.05)
+
+  # check some properties of the created objects
+  expect_equal(object = length(tau_hats), expected = Q_1)
+  expect_equal(object = is.numeric(crit_value_1), expected = TRUE)
+})
--- Code/PermFDATest/tests/testthat/test-means_test.R
+++ Code/PermFDATest/tests/testthat/test-means_test.R
@@ -82,7 +82,7 @@ test_that("Approximation of Critical Value for Means-based Test works", {
   )
 
   tstats <- means_tstats(
-    full = FALSE, approxQ = 100, sample1 = sample_1,
+    full = FALSE, approxQ = 250, sample1 = sample_1,
     sample2 = sample_2, interpolation_mode = "linear",
     domain = c(0, 1), grid = seq(0, 1, length.out = 100)
   )
@@ -90,4 +90,104 @@ test_that("Approximation of Critical Value for Means-based Test works", {
   crit_value <- crit_val(realizations = tstats, alpha = 0.05)
 
   expect_equal(object = 0, expected = 1)
+})
+
+test_that("Full Permutation Procedure works for mean based test for interpolation_mode = 'bspline'.", {
+  grid <- seq(from = 0, to = 1, length.out = 100)
+
+  # create two sample with two observations each for a trivial case
+  sample1 <- list(
+    list(
+      args = grid,
+      vals = rep(0, times = length(grid))
+    ),
+    list(
+      args = grid,
+      vals = rep(0.5, times = length(grid))
+    )
+  )
+  sample2 <- list(
+    list(
+      args = grid,
+      vals = rep(1, times = length(grid))
+    ),
+    list(
+      args = grid,
+      vals = rep(1.5, times = length(grid))
+    )
+  )
+  sample3 <- list(
+    list(
+      args = grid,
+      vals = grid
+    ),
+    list(
+      args = grid,
+      vals = grid
+    )
+  )
+
+  Q_1 <- choose(n = length(sample1) + length(sample2), k = length(sample1))
+
+  t_vals <- perm_tstats_full(
+    Q = Q_1, sample1 = sample1, sample2 = sample2,
+    t_stat_func = means_tstat, interpolation_mode = "bspline", domain = c(0, 1),
+    n_basis = 15
+  )
+  crit_value_1 <- crit_val(realizations = t_vals, alpha = 0.05)
+
+  diff_1 <- c(-1, -0.5, 0, 0, 0.5, 1)^2
+  t_real_1 <- quantile(x = diff_1, probs = 0.95, names = FALSE)
+
+  expect_equal(object = crit_value_1, expected = t_real_1)
+})
+
+test_that("Full Permutation Procedure works for mean based test for interpolation_mode = 'linear'.", {
+  grid <- seq(from = 0, to = 1, length.out = 100)
+
+  # create two sample with two observations each for a trivial case
+  sample1 <- list(
+    list(
+      args = grid,
+      vals = rep(0, times = length(grid))
+    ),
+    list(
+      args = grid,
+      vals = rep(0.5, times = length(grid))
+    )
+  )
+  sample2 <- list(
+    list(
+      args = grid,
+      vals = rep(1, times = length(grid))
+    ),
+    list(
+      args = grid,
+      vals = rep(1.5, times = length(grid))
+    )
+  )
+  sample3 <- list(
+    list(
+      args = grid,
+      vals = grid
+    ),
+    list(
+      args = grid,
+      vals = grid
+    )
+  )
+
+  Q_1 <- choose(n = length(sample1) + length(sample2), k = length(sample1))
+
+  t_vals_1 <- perm_tstats_full(
+    Q = Q_1, sample1 = sample1, sample2 = sample2,
+    t_stat_func = means_tstat, interpolation_mode = "linear", domain = c(0, 1),
+    grid = seq(0, 1, length.out = 1000)
+  )
+  crit_value_1 <- crit_val(realizations = t_vals_1, alpha = 0.05)
+
+  diff_1 <- c(-1, -0.5, 0, 0, 0.5, 1)^2
+  t_real_1 <- quantile(x = diff_1, probs = 0.95, names = FALSE)
+
+  expect_equal(object = crit_value_1, expected = t_real_1)
 })
