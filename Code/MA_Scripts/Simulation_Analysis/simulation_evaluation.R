12b95c4a28174a20c68ece8c6c04d17e92f78a67
Jakob Juergens <jakob.r.juergens@gmail.com>
2022-06-02 12:32:14 +0200

    Simulation 3

--- Code/MA_Scripts/Simulation_Analysis/simulation_evaluation.R
+++ Code/MA_Scripts/Simulation_Analysis/simulation_evaluation.R
@@ -20,8 +20,8 @@ sim_folders <- "../../Data/Cluster_Simulation_Outputs/"
 # set individual folder paths
 sim_1_path <- paste0(sim_folders, "Simulation_1/28_05_2022/")
 sim_2_path <- paste0(sim_folders, "Simulation_2/30_05_2022/")
-sim_3_path <- paste0(sim_folders, "Simulation_3/31_05_2022/")
-sim_4_path <- paste0(sim_folders, "Simulation_4/31_05_2022_test/")
+sim_3_path <- paste0(sim_folders, "Simulation_3/02_06_2022/")
+sim_4_path <- paste0(sim_folders, "Simulation_4/01_06_2022/")
 # set Output path
 output_path <- "../../Data/Cluster_Simulation_Outputs/Evaluation/"
 
